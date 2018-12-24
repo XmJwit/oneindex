8a5fa54a0133d5c21a9d06e72911d79f78766d60
TheZihanGu <39030261+TheZihanGu@users.noreply.github.com>
2018-12-24 19:08:39 +0800

    新增适用于Ubuntu和Debian的安装脚本

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=en_US.UTF-8
+
+echo "OneIndex 安装脚本，By TheZihanGu"
+echo "OneIndex 将安装在 /var/www/html"
+echo "此脚本仅适用于Ubuntu和Debian系统."
+echo "[必须]请使用root用户执行此脚本."
+echo "项目页面：https://github.com/donwa/oneindex"
+echo "Feedback：请到Issues反馈。"
+echo "请按任意键开始安装，或者Ctrl+C来取消安装."
+read
+echo "开始安装..."
+ln -sf bash /bin/sh
+apt-get update -y
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install git -y
+apt-get install unzip -y
+apt-get install php7.2 -y
+apt-get install php7.2-fpm php7.2-curl php7.2-json php7.2-mbstring php7.2-xml  php7.2-intl -y
+cd /var/www/
+git clone https://github.com/donwa/oneindex.git
+rm -rf /var/www/html/*
+mv oneindex html
+service restart apache2
+echo "安装完成。请前往 http://[IP]/ 进行程序初始化。"
